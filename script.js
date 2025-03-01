936ffc7b086cbdfeab63d7d981eb40879740407e
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-01 10:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ document.body.appendChild(getSumBtn);
 const getSum = () => {
 //Add your code here
 	let prices = document.querySelectorAll(".price")
-	console.log(prices)
+
+	let total = 0;
+
+	prices.forEach(price=>{
+		total += parseFloat(price.textContent)
+	})
+
+	let table = document.querySelector("table");
+    let newRow = document.createElement("tr");
+
+	newRow.innerHTML = `
+	<td><h3>Total</h3></td>
+	<td><h3>${total}</h3></td>
+	`
+    table.appendChild(newRow);
+	
   
 };
 
